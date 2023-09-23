8988bf3101f337a5082df80ee65e9ee815db4f3d
Fahad Abdul Qayyum <fahadabdulqayyum@focuspass.com>
2023-09-23 21:50:53 +0500

    Product's img and name designed within a card

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,38 +1,13 @@
 import React from 'react';
 import Products from './products.json'
-import { Row, Col, Card } from 'antd';
+import ProductItems from './ProductItems';
 
 const Product = () => {
 
-  const { Meta } = Card;
-
   return (
-    <span>
-      <h1>Products</h1>
-      <Row gutter={16}>
-        <Col span={8}>
-      {Products.map(v => (
-        // <div style={{display:'flex',flexDirection:'row'}}>
-        //   <Card
-        //     hoverable
-        //     style={{ width: 240 }}
-        //     cover={<img alt="example" src={v.img} />}
-        //   >
-        //     <Meta title={v.name} description="www.instagram.com" />
-        //   </Card>
-        //  </div>
-
-          <Card 
-          title={v.name} 
-          bordered={false}
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt="example" src={v.img} />}>
-          </Card>
-        
-      ))}
-      </Col>
-      </Row>
+    <span className='flex space-x-2 m-5'>
+          {Products.map(item => <ProductItems key={item.id} items={item}/>
+          )}
     </span>
 
   )
---
+++ src/components/product/ProductItems.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Row, Col, Card } from 'antd';
+
+const ProductItems = ({items}) => {
+
+    const { Meta } = Card;
+
+    return (
+        <Card
+            hoverable
+            style={{
+                width: 240,
+            }}
+            cover={<img alt="example" src={items.img} />}
+        >
+            <Meta title={items.name} description="www.instagram.com" />
+        </Card>
+    )
+}
+
+export default ProductItems
