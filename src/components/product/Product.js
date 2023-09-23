e9dcaff136defbb40b94f585c77fd88b4dc68eca
Fahad Abdul Qayyum <fahadabdulqayyum@focuspass.com>
2023-09-23 20:10:00 +0500

    Images, desc, and amount designed

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React from 'react';
+import Products from './products.json'
+import { Row, Col, Card } from 'antd';
 
 const Product = () => {
+
+  const { Meta } = Card;
+
   return (
-    <div>Product</div>
+    <span>
+      <h1>Products</h1>
+      <Row gutter={16}>
+        <Col span={8}>
+      {Products.map(v => (
+        // <div style={{display:'flex',flexDirection:'row'}}>
+        //   <Card
+        //     hoverable
+        //     style={{ width: 240 }}
+        //     cover={<img alt="example" src={v.img} />}
+        //   >
+        //     <Meta title={v.name} description="www.instagram.com" />
+        //   </Card>
+        //  </div>
+
+          <Card 
+          title={v.name} 
+          bordered={false}
+          hoverable
+          style={{ width: 240 }}
+          cover={<img alt="example" src={v.img} />}>
+          </Card>
+        
+      ))}
+      </Col>
+      </Row>
+    </span>
+
   )
 }
 
