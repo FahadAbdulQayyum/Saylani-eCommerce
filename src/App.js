02afa96b5c4dc04fde036abc314b7567defad952
Fahad Abdul Qayyum <fahadabdulqayyum@focuspass.com>
2023-09-25 19:39:15 +0500

    Hopefully the whole firebase set is up

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import {
 } from "react-router-dom";
 import Home from './components/home/Home'
 import Product from './components/product/Product';
-import storage from "./firebaseConfig.js"
-import {ref} from "firebase/storage"
-
+import storage from './components/firebase/firebaseConfig'
+import {
+    ref,
+    uploadBytesResumable,
+    getDownloadURL 
+} from "firebase/storage";
 
 const App = () => {
 
+  const [percent, setPercent] = useState(0);
   const [file, setFile] = useState("");
 
   const router = createBrowserRouter([
@@ -25,19 +29,87 @@ const App = () => {
   ]);
 
 // Handles input change event and updates state
-const handleChange = event => {
-  if(!event.target.files[0]){
-    return alert("Please choose a file first");
-  }
-  const storageRef = ref(storage, `/files/${file.name}`)
-  setFile(event.target.files[0]);
-}
+// const handleChange = event => {
+//   if(!event.target.files[0]){
+//     return alert("Please choose a file first");
+//   }
+//   const storageRef = ref(storage, `/files/${file.name}`)
+//   setFile(event.target.files[0]);
+// }
+
+function handleChange(event) {
+        setFile(event.target.files[0]);
+    }
+
+// function handleUpload() {
+//     if (!file) {
+//         alert("Please choose a file first!")
+//     }
+ 
+//     const storageRef = ref(storage,`/files/${file.name}`)
+//     const uploadTask = uploadBytesResumable(storageRef, file);
+ 
+//     uploadTask.on(
+//         "state_changed",
+//         (snapshot) => {
+//             const percent = Math.round(
+//                 (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+//             );
+ 
+//             // update progress
+//             setPercent(percent);
+//         },
+//         (err) => console.log(err),
+//         () => {
+//             // download url
+//             getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+//                 console.log(url);
+//             });
+//         }
+//     ); 
+// }
+
+  const handleUpload = () => {
+        if (!file) {
+            alert("Please upload an image first!");
+        }
+ 
+        const storageRef = ref(storage, `/files/${file.name}`);
+ 
+        // progress can be paused and resumed. It also exposes progress updates.
+        // Receives the storage reference and the file to upload.
+        const uploadTask = uploadBytesResumable(storageRef, file);
+ 
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const percent = Math.round(
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                );
+ 
+                // update progress
+                setPercent(percent);
+            },
+            (err) => console.log(err),
+            () => {
+                // download url
+                getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+                    console.log(url);
+                });
+            }
+        );
+    };
 
 return (
   <div className='flex flex-col'>
       <RouterProvider router={router} />
-      <input type="file" accept="image/*" onChange={handleChange}/>
-      <button className='bg-slate-600 text-white py-2 w-52 mt-3'>Upload to Firebase</button>
+      {/* <input type="file" accept="image/*" onChange={handleChange}/>
+      <button className='bg-slate-600 text-white py-2 w-52 mt-3'>Upload to Firebase</button> */}
+        <div>
+            <input type="file" onChange={handleChange} accept="" />
+            <button onClick={handleUpload}>Upload to Firebase</button>
+            <p>{percent} "% done"</p>
+        </div>
   </div>
 );
 
--- src/components/firebase/firebaseConfig.js
+++ src/components/firebase/firebaseConfig.js
@@ -3,13 +3,21 @@ import { getStorage } from "firebase/storage";
  
 // Initialize Firebase
 const app = initializeApp ({
-    apiKey: <apiKey>,
-    authDomain: <authDomain>,
-    projectId: <projectId>,
-    storageBucket: <storageBucket>,
-    messagingSenderId: <messagingSenderId>,
-    appId: <appId>,
-    measurementId: <measurementId>,
+    apiKey: "AIzaSyAERQbwUhP5ttlnRtsu2hgTt4BALZ8ynyc",
+  authDomain: "saylani-ecommerce-da434.firebaseapp.com",
+  projectId: "saylani-ecommerce-da434",
+  storageBucket: "saylani-ecommerce-da434.appspot.com",
+  messagingSenderId: "965964086692",
+  appId: "1:965964086692:web:9e73b73221106232d5d531",
+  measurementId: "G-971PN0HHF5"
+    
+    // apiKey: <apiKey>,
+    // authDomain: <authDomain>,
+    // projectId: <projectId>,
+    // storageBucket: <storageBucket>,
+    // messagingSenderId: <messagingSenderId>,
+    // appId: <appId>,
+    // measurementId: <measurementId>,
 });
  
 // Firebase storage reference
