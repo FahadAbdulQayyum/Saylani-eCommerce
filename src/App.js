e27cb21092ebfceaa359b6aed41892d39860559e
Fahad Abdul Qayyum <fahadabdulqayyum@focuspass.com>
2023-09-25 19:44:01 +0500

    Now finally, the firebase is uploading successfully the images, for me literally now it seems a like I have achieved a milestone

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import {
   createBrowserRouter,
   RouterProvider,
@@ -7,9 +7,9 @@ import Home from './components/home/Home'
 import Product from './components/product/Product';
 import storage from './components/firebase/firebaseConfig'
 import {
-    ref,
-    uploadBytesResumable,
-    getDownloadURL 
+  ref,
+  uploadBytesResumable,
+  getDownloadURL
 } from "firebase/storage";
 
 const App = () => {
@@ -20,99 +20,94 @@ const App = () => {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home/>
+      element: <Home />
     },
     {
       path: "/product",
-      element: <Product/>
+      element: <Product />
     },
   ]);
 
-// Handles input change event and updates state
-// const handleChange = event => {
-//   if(!event.target.files[0]){
-//     return alert("Please choose a file first");
-//   }
-//   const storageRef = ref(storage, `/files/${file.name}`)
-//   setFile(event.target.files[0]);
-// }
+  // Handles input change event and updates state
+  // const handleChange = event => {
+  //   if(!event.target.files[0]){
+  //     return alert("Please choose a file first");
+  //   }
+  //   const storageRef = ref(storage, `/files/${file.name}`)
+  //   setFile(event.target.files[0]);
+  // }
 
-function handleChange(event) {
-        setFile(event.target.files[0]);
-    }
+  function handleChange(event) {
+    setFile(event.target.files[0]);
+  }
 
-// function handleUpload() {
-//     if (!file) {
-//         alert("Please choose a file first!")
-//     }
- 
-//     const storageRef = ref(storage,`/files/${file.name}`)
-//     const uploadTask = uploadBytesResumable(storageRef, file);
- 
-//     uploadTask.on(
-//         "state_changed",
-//         (snapshot) => {
-//             const percent = Math.round(
-//                 (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-//             );
- 
-//             // update progress
-//             setPercent(percent);
-//         },
-//         (err) => console.log(err),
-//         () => {
-//             // download url
-//             getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-//                 console.log(url);
-//             });
-//         }
-//     ); 
-// }
+  // function handleUpload() {
+  //     if (!file) {
+  //         alert("Please choose a file first!")
+  //     }
+
+  //     const storageRef = ref(storage,`/files/${file.name}`)
+  //     const uploadTask = uploadBytesResumable(storageRef, file);
+
+  //     uploadTask.on(
+  //         "state_changed",
+  //         (snapshot) => {
+  //             const percent = Math.round(
+  //                 (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+  //             );
+
+  //             // update progress
+  //             setPercent(percent);
+  //         },
+  //         (err) => console.log(err),
+  //         () => {
+  //             // download url
+  //             getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+  //                 console.log(url);
+  //             });
+  //         }
+  //     ); 
+  // }
 
   const handleUpload = () => {
-        if (!file) {
-            alert("Please upload an image first!");
-        }
- 
-        const storageRef = ref(storage, `/files/${file.name}`);
- 
-        // progress can be paused and resumed. It also exposes progress updates.
-        // Receives the storage reference and the file to upload.
-        const uploadTask = uploadBytesResumable(storageRef, file);
- 
-        uploadTask.on(
-            "state_changed",
-            (snapshot) => {
-                const percent = Math.round(
-                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                );
- 
-                // update progress
-                setPercent(percent);
-            },
-            (err) => console.log(err),
-            () => {
-                // download url
-                getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-                    console.log(url);
-                });
-            }
+    if (!file) {
+      alert("Please upload an image first!");
+    }
+
+    const storageRef = ref(storage, `/files/${file.name}`);
+
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const percent = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
         );
-    };
 
-return (
-  <div className='flex flex-col'>
+        // update progress
+        setPercent(percent);
+      },
+      (err) => console.log(err),
+      () => {
+        // download url
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          console.log(url);
+        });
+      }
+    );
+  };
+
+  return (
+    <div className='flex flex-col'>
       <RouterProvider router={router} />
-      {/* <input type="file" accept="image/*" onChange={handleChange}/>
-      <button className='bg-slate-600 text-white py-2 w-52 mt-3'>Upload to Firebase</button> */}
-        <div>
-            <input type="file" onChange={handleChange} accept="" />
-            <button onClick={handleUpload}>Upload to Firebase</button>
-            <p>{percent} "% done"</p>
-        </div>
-  </div>
-);
-
+      <div>
+        <input type="file" accept="image/*" onChange={handleChange} />
+        <button className='bg-slate-600 text-white py-2 w-52 mt-3' onClick={handleUpload}>Upload to Firebase</button>
+        <p>{percent}"% done"</p>
+      </div>
+    </div>
+  );
 }
 
 export default App
