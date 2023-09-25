3b2fb4527ff7807d75bbd5c79a32d1795ce40851
Fahad Abdul Qayyum <fahadabdulqayyum@focuspass.com>
2023-09-25 18:08:41 +0500

    firebase function created for uploading images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {
   createBrowserRouter,
   RouterProvider,
@@ -7,6 +7,8 @@ import Home from './components/home/Home'
 import Product from './components/product/Product';
 
 const App = () => {
+
+  const [file, setFile] = useState("");
 
   const router = createBrowserRouter([
     {
@@ -19,9 +21,19 @@ const App = () => {
     },
   ]);
 
-  return (
-    <RouterProvider router={router} />
-  )
+// Handles input change event and updates state
+const handleChange = event => {
+  setFile(event.target.files[0]);
+}
+
+return (
+  <div className='flex flex-col'>
+      <RouterProvider router={router} />
+      <input type="file" accept="image/*" onChange={handleChange}/>
+      <button className='bg-slate-600 text-white py-2 w-52 mt-3'>Upload to Firebase</button>
+  </div>
+);
+
 }
 
 export default App
