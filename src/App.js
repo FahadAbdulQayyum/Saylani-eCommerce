ab120c4fbc96a7fad62fcfa9fc8cf06ed7f0218c
Fahad Abdul Qayyum <fahadabdulqayyum@focuspass.com>
2023-09-26 15:41:44 +0500

    product's price input field highlighted via comment

--- src/App.js
+++ src/App.js
@@ -70,6 +70,7 @@ const App = () => {
         <input type="text" className='border my-1 p-2' placeholder="Enter your product's Name"/><br/>
         {/* Product's description input field */}
         <input type="text" className='border my-1 p-2' placeholder="Enter your product's Desc"/><br/>
+        {/* Product's price input field */}
         <input type="Number" className='border my-1 p-2' placeholder="Enter your product's Price"/><br/>
         <input type="file" accept="image/*" onChange={handleChange} />
         <button className='bg-slate-600 text-white py-2 w-52 mt-3' onClick={handleUpload}>Upload to Firebase</button>
