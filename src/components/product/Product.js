3881912568c65ac67479dcc7c9a113d41563d8d8
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2023-10-01 16:52:54 +0500

    grid also got adjusted accordingly

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react';
 import Products from './products.json'
 // ProductItems imported for aligning and styling the products' array in another component for better readibility
 import ProductItems from './ProductItems';
-// import ProductItems from './ProductItems copy';
 import { getData } from '../contextApi/getData';
 
 const Product = () => {
@@ -15,17 +14,17 @@ const Product = () => {
       setData(dataa)
     }
     dataFetch()
-    console.log('getData', data)
-    // setData(getData())
   }, [])
 
   return (
-    <span className='flex space-x-5 m-5'>
-      {/*  Products' array is spread using map */}
-      {[...data,...Products].map(item => <ProductItems key={item.id} items={item} />
-      )}
-      {/* {Products.map(item => <ProductItems key={item.id} items={item} />
-      )} */}
+    // <span className='grid grid-rows-3 space-x-5 m-5 '>
+    // <span className='grid space-x-5 m-5 '>
+    <span className='flex space-x-5 m-5 '>
+      <span className='grid grid-cols-4 gap-10'>
+        {/*  Products' array is spread using map */}
+        {[...data, ...Products].map(item => <ProductItems key={item.id} items={item} />
+        )}
+      </span>
     </span>
 
   )
