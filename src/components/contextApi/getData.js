12a1fe1b2a46636198a820c4311b710ff8c3f90b
Fahad Abdul Qayyum <100997736+FahadAbdulQayyum@users.noreply.github.com>
2023-10-06 16:14:22 +0500

    Update getData.js
    txtDb specified via comment

--- src/components/contextApi/getData.js
+++ src/components/contextApi/getData.js
@@ -4,7 +4,7 @@ import { txtDb } from '../firebase/firebaseConfig';
 import { collection, getDocs } from 'firebase/firestore';
 
 export const getData = async () => {
-    // This gets the collection stored in the firebasestore
+    // This gets the collection stored in the firebasestore in the name of txtDb
     const valRef = collection(txtDb, 'txtData')
     const dataDb = await getDocs(valRef)
     const allData = dataDb.docs.map(val => ({ ...val.data(), id: val.id }))
