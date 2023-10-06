f19425449e893fa29dd055c94cb0b0fe0f226892
Fahad Abdul Qayyum <100997736+FahadAbdulQayyum@users.noreply.github.com>
2023-10-06 16:15:06 +0500

    Update getData.js
    refVal explained via comment

--- src/components/contextApi/getData.js
+++ src/components/contextApi/getData.js
@@ -6,6 +6,7 @@ import { collection, getDocs } from 'firebase/firestore';
 export const getData = async () => {
     // This gets the collection stored in the firebasestore in the name of txtDb
     const valRef = collection(txtDb, 'txtData')
+    // Get the data present in the collection and store them into dataDb
     const dataDb = await getDocs(valRef)
     const allData = dataDb.docs.map(val => ({ ...val.data(), id: val.id }))
     console.log('allData', allData);
