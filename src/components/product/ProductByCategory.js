b67b42a8bd67b67839fc3840a81d961af8376a3d
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2023-10-07 22:58:29 +0500

    category based representation done with price float

--- src/components/product/ProductByCategory.js
+++ src/components/product/ProductByCategory.js
@@ -11,13 +11,40 @@ const ProductByCategory = () => {
         }
         dataFetch()
     }, [])
+
+    const ProductByCat = ({cat, name}) => {
+        console.log('cat',cat)
+        const filtered = data.filter(v=>v.prodCat === name)
+        console.log('filtered', filtered)
+        return (
+            <div className='relative'>
+                <h2 className='bg-teal-200'>{cat.prodCat === name && name}</h2>
+                {cat.prodCat === name && <span>
+                    <img className='h-36' src={cat.imgUrl}/>
+                    <small>{cat.prodName}</small>
+                    <small className='absolute top-5 left-0 z-10 text-white bg-orange-400 px-3'>{'$'+cat.prodPrice}</small>
+                </span>}
+            </div>
+        )
+
+        // return (
+        //     <>
+        //         {
+        //             filtered.map(v=><>{v.prodName}</>)
+        //         }
+        //     </>
+        // )
+    }
+
     return (
         <div>
             {
                 data.map(v=>
                     <span key={v.id}>
-                        <h3>{v?.prodCat}</h3>
-                        {console.log('v.id',v?.prodCat)}
+                        <h3>{v?.prodCat === 'Vegetable'}</h3>
+                        <ProductByCat cat={v} name='Vegetable'/>
+                        <ProductByCat cat={v} name='Sweet'/>
+                        <ProductByCat cat={v} name='Fruit'/>
                     </span>
                     )
             }
