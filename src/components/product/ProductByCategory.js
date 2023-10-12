2129a55f8e57a90b85bab992ca8747e328473cc1
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2023-10-13 01:37:43 +0500

    Images brought on homepage and aligned into cards

--- src/components/product/ProductByCategory.js
+++ src/components/product/ProductByCategory.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react'
 // getData imported from 'getData ContextApi' for making it reuseable once data is retrieved
 import { getData } from '../contextApi/getData';
+import ProductItems from './ProductItems';
 
 // ProductByCategory arrow function starts here
 const ProductByCategory = () => {
@@ -21,23 +22,31 @@ const ProductByCategory = () => {
     // Remove the duplicacy
     let unrepeatedName = [...new Set(data.map(v => v.prodCat))];
     // Remove the 'undefined'
-    let unrepeatedCat = unrepeatedName.filter(v=>v!==undefined)
-    
+    let unrepeatedCat = unrepeatedName.filter(v => v !== undefined)
+
     // Categorize the object based on it's splitted category
-    let arr = unrepeatedCat.map(name => {
+    let alignedBasedCategory = unrepeatedCat.map(name => {
         // filter the data having the same category name
         let prod = data.filter(v => v.prodCat === name);
         // Return the category name and product name
         return { name, prod };
     });
-    
+
     // Console the arr to notice the change
-    console.log('arr', arr);
-    
+    console.log('alignedBasedCategory', alignedBasedCategory);
 
     return (
-    <div>ProductByCategory</div>
-  )
+        <div>
+            {
+                alignedBasedCategory?.map(v => <div key={v.id}>
+                    <h3 className='font-bold bg-red-400 flex justify-center text-white py-2'>{v.name}</h3>
+                    <p className='flex flex-row'>{v.prod.map(p => <span className='flex flex-row'><span className='grid grid-cols-2'>{
+                        <ProductItems key={p?.prod?.id} items={p} />
+                    }</span></span>)}</p>
+                </div>)
+            }
+        </div>
+    )
 }
 
 // ProductByCategory exported
