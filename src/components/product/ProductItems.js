1982fd6221f4ec02a3c7ae7be6df0e18788b544f
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2023-10-16 14:00:24 +0500

    cover's purpose highlighted via comment

--- src/components/product/ProductItems.js
+++ src/components/product/ProductItems.js
@@ -18,7 +18,7 @@ console.log('itemsss',items)
     // Card's backgroundImage is initialized here
     backgroundImage: `url(${items.imgUrl})`,
     paddingTop: '100%', // Maintain a square aspect ratio
-    backgroundSize: 'cover',
+    backgroundSize: 'cover', // Making the image as cover
     backgroundPosition: 'center',
   };
 
