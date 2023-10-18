96ef7628c2dc8ba62d28b67819f88377f889b888
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2023-10-18 14:21:27 +0500

    initializedApp import explained via comments

--- src/components/firebase/firebaseConfig.js
+++ src/components/firebase/firebaseConfig.js
@@ -1,3 +1,4 @@
+// initializeApp imported from firebase/App
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from 'firebase/firestore';
