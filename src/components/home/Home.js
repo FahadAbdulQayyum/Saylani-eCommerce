1599fed53b66b335271f037d0a17e02ccfc1a099
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2023-10-20 14:54:23 +0500

    Product import explained via comments

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,8 @@
 // React is imported from 'react';
 import React from 'react'
-// Navbar is imported from './navbar/Navbar' directory
+// Navbar is imported from '../navbar/Navbar' directory
 import Navbar from '../navbar/Navbar'
+// Product is imported from '../product/Product' directory
 import Product from '../product/Product'
 import ProductByCategory from '../product/ProductByCategory'
 
